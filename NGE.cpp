a8ffeb9c3c52755ceb672632d8368d6abf3ed92d
balkishan bajpay <bkbajpay0609@gmail.com>
2024-09-19 14:15:25 +0530

    updated

--- NGE.cpp
+++ NGE.cpp
@@ -9,7 +9,7 @@ private:
     int minimum, index, distance[53], stat[53];
     int source;
     int graph[53][53];  
-    vector<string> stations;  
+    vector <string> stations;
 
 public:
     BMP() {
@@ -90,10 +90,8 @@ public:
         }
     }
     void displayStations();
-    void addStation(string newStation);
     void viewStation(int index);
     void updateStation(int index, string newName);
-    void deleteStation(int index);
     void sourcestinput();
     int mindistance(int distance[], bool stat[]);
     void dijkstra(int graph[53][53], int source);
@@ -106,15 +104,6 @@ public:
             cout << i << ". " << stations[i] << endl;
         }
         cout << endl;
-    }
-
-    void BMP::addStation(string newStation) {
-        if (stations.size() < 53) {
-            stations.push_back(newStation);
-            cout << "Station " << newStation << " added successfully.\n";
-        } else {
-            cout << "Cannot add more stations. Limit reached.\n";
-        }
     }
 
     void BMP::viewStation(int index) {
@@ -130,19 +119,6 @@ public:
         if (index >= 0 && index < stations.size()) {
             cout << "Updating station " << stations[index] << " to " << newName << endl;
             stations[index] = newName;
-        } else {
-            cout << "Invalid station index.\n";
-        }
-    }
-
-    void BMP::deleteStation(int index) {
-        if (index >= 0 && index < stations.size()) {
-            cout << "Deleting station: " << stations[index] << endl;
-            stations.erase(stations.begin() + index);
-            for (int i = 0; i < 53; i++) {
-                graph[index][i] = 0;
-                graph[i][index] = 0;
-            }
         } else {
             cout << "Invalid station index.\n";
         }
@@ -201,7 +177,7 @@ int main() {
     
     while (true) {
         cout << "\nMenu";
-        cout<<"\n1. Display all stations\n2. Add a station\n3. View a station\n4. Update a station\n5. Delete a station\n6. Find shortest path\n7. Exit\n";
+        cout<<"\n1. Display all stations\n2. View a station\n3. Update a station\n4. Find minimum number of stations from source st. to all other stations \n5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -210,31 +186,21 @@ int main() {
                 b1.displayStations();
                 break;
             case 2:
-                cout << "Enter the new station name: ";
-                cin >> newStation;
-                b1.addStation(newStation);
-                break;
-            case 3:
                 cout << "Enter the station index to view: ";
                 cin >> index;
                 b1.viewStation(index);
                 break;
-            case 4:
+            case 3:
                 cout << "Enter the station index to update: ";
                 cin >> index;
                 cout << "Enter the new station name: ";
                 cin >> newName;
                 b1.updateStation(index, newName);
                 break;
-            case 5:
-                cout << "Enter the station index to delete: ";
-                cin >> index;
-                b1.deleteStation(index);
-                break;
-            case 6:
+            case 4:
                 b1.sourcestinput();
                 break;
-            case 7:
+            case 5:
                 cout << "Exiting...\n";
                 return 0;
             default:
